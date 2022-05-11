44f1ca914a7fddedcac5e891369830ad962307e2
sian-0018 <56289107+sian-0018@users.noreply.github.com>
2022-05-11 20:55:11 +0800

    w05

---
+++ data.js
@@ -0,0 +1,32 @@
+const students = [
+    {
+        id: 1,
+        name: 'Tom',
+        score: 90,
+        favoriteAnimal: 'cat'
+    },
+    {
+        id: 2,
+        name: 'Jane',
+        score: 68,
+        favoriteAnimal: 'dog'
+    },
+    {
+        id: 3,
+        name: 'Helen',
+        score: 80,
+        favoriteAnimal: 'cat'
+    },
+    {
+        id: 4,
+        name: 'Tommy',
+        score: 95,
+        favoriteAnimal: 'snake'
+    },
+    {
+        id: 5,
+        name: 'Lucy',
+        score: 55,
+        favoriteAnimal: 'frog'
+    },
+]
