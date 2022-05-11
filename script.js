6cbbe8a7f3f6372445f882a5e5bdb7b02bb6e1f6
sian-0018 <56289107+sian-0018@users.noreply.github.com>
2022-05-11 20:55:38 +0800

    w08

--- script.js
+++ script.js
@@ -1,41 +1,111 @@
-let secretNumber = 56;
-let score = 20;
-let highscore = 0;
+const main = document.querySelector('#main');
+const addUserBtn = document.querySelector('#add-user');
+const doubleBtn = document.querySelector('#double');
+const showMillionairesBtn = document.querySelector('#show-millionaires');
+const sortBtn = document.querySelector('#sort');
+const calculateBtn = document.querySelector('#calculate-wealth');
 
-const displayMessage = (message) => {
-  document.querySelector('.message').textContent = message;
-};
+let data = [];
+
+const config = {
+    numRandom: 5,
+    ratio: 1.5,
+    showCondition: 2000000
+}
+
+// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+
+function formatMoney(number) {
+
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+
+}
+
+const updateDOM = (providedData = data) => {
+    let tempData = providedData.map((item) => {
+        return`<div class="person"><strong>${item.name}</strong> $${formatMoney(item.money)}</div>`
+    });
+    tempData = tempData.join('');
+    //console.log('tempData', tempData);
+    main.innerHTML = `<h2><strong>Person</strong> Wealth</h2> ${tempData}`
+}
 
 
-document.querySelector('.check').addEventListener('click', () => {
-  const guess = Number(document.querySelector('.guess').value);
+const addData = (obj) => {
+    data.push(obj);
+    //console.log('data',data);
+}
 
-  if (!guess) {
-    displayMessage('Need a number');
-    } else if (guess === secretNumber) {
-    displayMessage('Victor');
-    document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('body').style.backgroundColor = 'green';
-    if (score > highscore) {
-      highscore = score;
-      document.querySelector('.highscore').textContent = highscore;
+const getRandomUser = async () => {
+    const res = await fetch('https://randomuser.me/api');
+    const data = await res.json();
+    //console.log('random user data', data)
+    const user = data.results[0];
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random()*10000000)
+    } 
+    addData(newUser);
+    updateDOM();
+}
+
+const getUsers = () => {
+    for(let i=0; i < config.numRandom; i++){
+        getRandomUser();
     }
-   } else if (guess !== secretNumber) {
-    if (score > 1) {
-      displayMessage(guess > secretNumber ? 'Too high' : 'Too low');
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      displayMessage('Lose');
-      document.querySelector('.score').textContent = 0;
-    }
-  }
-});
-document.querySelector('.again').addEventListener('click',() => {
-  score = 20;
-  secretNumber = 66;
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('.guess').value = '';
-  displayMessage('Start guessing...');
-});
+}
+
+getUsers();
+
+// getRandomUser();
+// getRandomUser();
+// getRandomUser();
+
+const changeBtnText = () => {
+    addUserBtn.textContent = `Add User (${config.numRandom}) 👱‍♂️`;
+    doubleBtn.textContent = `Raise Money * ${config.ratio} 💰`;
+    showMillionairesBtn.textContent = `show > ${config.showCondition} 💵`;
+}
+
+changeBtnText();
+
+const doubleMoney = () => {
+    data = data.map( (user) => {
+        return {
+            name: user.name,
+            money: user.money * config.ratio
+        }
+    });
+    updateDOM();
+}
+
+const showMillionaries = () => {
+    const filteredData = data.filter( (user) => {
+        return user.money > config.showCondition;
+    });
+    //console.log('filteredData', filteredData);
+    updateDOM(filteredData);
+}
+
+const sortByRichest = () => {
+    data.sort( (a,b) => {
+        return b.money - a.money
+    });
+    updateDOM();
+}
+
+const calculateWealth = () => {
+    const wealth = data.reduce((wealth, user, index) =>{
+        console.log('total, index, money',wealth, index, user.money);
+        return wealth += user.money
+    }, 0)
+    main.innerHTML = main.innerHTML + `<h3>Total Wealth: <strong>$${formatMoney(wealth)}</strong></h3>`
+}
+
+calculateBtn.addEventListener('click', calculateWealth);
+addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
+showMillionairesBtn.addEventListener('click', showMillionaries);
+
+
+sortBtn.addEventListener('click', sortByRichest);
